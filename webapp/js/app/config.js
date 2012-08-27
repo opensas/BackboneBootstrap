f26c61a710cff14f2bf7ea4b0623d956711d4aac
opensas <opensas@gmail.com>
2012-08-27 04:46:28 -0300

    fixes #23 - modify config.js to infer when it's running on openshift

--- webapp/js/app/config.js
+++ webapp/js/app/config.js
@@ -1,8 +1,27 @@
-/*globals $,_,Backbone,utils,confirm,alert*/
+/*globals console*/
+'use strict';
 
 var app = app || {};
 
-app.config = {
-  // endpoint: 'http://localhost:9000/api/wines'
-  endpoint: 'http://bb-jugar.rhcloud.com/api/wines'
-}
+(function() {     // use an anonymous autoexec function to avoid polluting app.inferEndpoint
+
+  // returns a string identifying the environment in which the app is running
+  // possible values: local, openshift
+  var inferEndpoint = function() {
+    var location = window.location;
+    // running on openshift
+    if (location.hostname.indexOf('rhcloud')!==-1) {
+      return location.origin + '/api/wines';
+    // running locally
+    } else {   // if (location.protocol === "file:") {
+      return 'http://localhost:9000/api/wines';
+    }
+  };
+
+  app.config = {
+    endpoint: inferEndpoint()
+    // endpoint: 'http://localhost:9000/api/wines'
+    // endpoint: 'http://bb-jugar.rhcloud.com/api/wines'
+  };
+
+}());
