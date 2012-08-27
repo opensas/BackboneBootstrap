63217f4a33402af77d70150011692c149ebf3847
opensas <opensas@gmail.com>
2012-08-27 00:27:58 -0300

    fixes #18 - add initial test support using jasmine https://github.com/opensas/BackboneBootstrap/issues/18

---
+++ webapp/js/src/test/spec/stringHelper.js
@@ -0,0 +1,20 @@
+/*globals beforeEach*/
+'use strict';
+
+beforeEach(function() {
+  this.addMatchers({
+    toStartWith: function(text) {
+      return (this.actual.indexOf(text)===0);
+    }
+  });
+  this.addMatchers({
+    toEndWith: function(text) {
+      // private helpser function - don't want to mess with String prototype
+      var endsWith = function(text, suffix) {
+        return (text.indexOf(suffix, text.length - suffix.length) !== -1);
+      }
+      // return (this.actual.match(text+'$')!==null);
+      return endsWith(this.actual, text);
+    }
+  });
+});
