e051eb89ab090e7fa63bb591dacdcd0748a05134
opensas <opensas@gmail.com>
2012-09-07 02:24:55 -0300

    fix tabulation

--- webapp/js/src/tests/main.js
+++ webapp/js/src/tests/main.js
@@ -1,10 +1,10 @@
-    require(
-      [
-        'jasmine-html', 
-        'spec/convert.spec',
-        'spec/crud.spec'
-      ],
-      function() {
-        jasmine.getEnv().addReporter(new jasmine.HtmlReporter());
-        jasmine.getEnv().execute();
-      });
+require(
+  [
+    'jasmine-html', 
+    'spec/convert.spec',
+    'spec/crud.spec'
+  ],
+function() {
+  jasmine.getEnv().addReporter(new jasmine.HtmlReporter());
+  jasmine.getEnv().execute();
+});
