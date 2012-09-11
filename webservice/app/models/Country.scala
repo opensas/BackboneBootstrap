cdb94b4060222afbfb0be0be30e4a8290a31a255
opensas <opensas@gmail.com>
2012-09-11 03:19:23 -0300

    refs #41 - refactor country and wine model, create base Entity class, extract common behaviour

--- webservice/app/models/Country.scala
+++ webservice/app/models/Country.scala
@@ -18,13 +18,19 @@ case class Country (
 
   val code: String = "NN",
   val name: String = "unknown country"
-) {
+)
+  extends Entity
+{
   def update()  = Country.update(this)
   def save()    = Country.save(this)
   def delete()  = Country.delete(this)
 }
 
-object Country {
+object Country extends EntityCompanion[Country] {
+
+  val tableName = "country"
+
+  val defaultOrder = "name"
 
   val filterFields = List("code", "name")
 
@@ -36,78 +42,6 @@ object Country {
         id, code, name
       )
     }
-  }
-
-  def findById(id: Long): Option[Country] = {
-    DB.withConnection { implicit connection =>
-      SQL(
-        "select * from Country where id = {id}"
-      ).on(
-        'id   -> id
-      ).as(Country.simpleParser.singleOpt)
-    }
-  }
-
-  def find(query: Map[String, Seq[String]]): List[Country] = {
-    val (page, len, order, filter) = Http.parseQuery(query)
-    find(page, len, order, filter)
-  }
-
-  def count(query: Map[String, Seq[String]]): Long = {
-    val (page, len, order, filter) = Http.parseQuery(query)
-    count(filter)
-  }
-
-  def find(
-    page: Int = 1, len: Int = Http.DEFAULT_PAGE_LEN, 
-    order: String = "name", filter: String = "", condition: String = ""
-  ): List[Country] = {
-    findWithParser(fields="*", page=page, len=len, order=order, 
-      filter=filter, condition=condition, parser=Country.simpleParser *
-    )
-  }
-
-  def count(filter: String = "", condition: String = ""): Long = {
-    findWithParser(fields="count(*)", page=1, len=1, order = "", 
-      filter=filter, condition=condition, parser=scalar[Long].single
-    )
-  }
-
-  private def findWithParser[T](
-    page: Int = 1, len: Int = Http.DEFAULT_PAGE_LEN,
-    order: String = "name", filter: String = "", condition: String = "", fields: String = "*",
-    parser: ResultSetParser[T]
-  ): T = {
-    DB.withConnection { implicit connection =>
-
-      val where = {
-        var conditions: List[String] = List()
-        if (filter != "") {
-          conditions ::=
-            this.filterFields
-            .map( field => "lower(%s) like lower({filter})".format(field) )
-            .mkString(" or ")
-        }
-        if (condition != "") conditions ::= condition
-        if (conditions.length > 0) {
-          "where (" + conditions.mkString(") and (") + ")"
-        } else {
-          ""
-        }
-      }
-
-      val orderBy = if (order == "") "" else "order by " + order
-      val sql = "select %s from Country %s %s limit {offset}, {len}"
-
-      SQL(
-        sql.format(fields, where, orderBy)
-      ).on(
-        'offset     -> (page-1) * len,
-        'len        -> len,
-        'filter     -> ("%"+filter.toLowerCase()+"%")
-      ).as(parser)
-    }
-
   }
 
   def validate(country: Country): List[Error] = {
@@ -201,20 +135,6 @@ object Country {
         }
 
       }
-    }
-  }
-
-  def delete(country: Country): Unit = {
-    delete(country.id)
-  }
-
-  def delete(id: Pk[Long]): Unit = {
-    id.map { id => delete(id) }
-  }
-
-  def delete(id: Long): Unit = {
-    DB.withConnection { implicit connection =>
-      SQL("delete from Country where id = {id}").on('id -> id).executeUpdate()
     }
   }
 
