b4907e2cbc87998694ea260d12acbb1151241f78
opensas <opensas@gmail.com>
2013-03-04 00:39:39 -0300

    add Reviews and Wines with Reviews (master-detail example) options to main menu

--- webservice/conf/evolutions/default/14.sql
+++ webservice/conf/evolutions/default/14.sql
@@ -8,6 +8,9 @@ INSERT INTO Menu VALUES
   (10, 'menu', null, 02, 'Wines',             '', '#',         '', '#Menu.Wines', true) ,
   (11, 'menu', 10,   01, 'Wines',             '', 'Wine',      '', '#Menu.Wines', true) ,
   (12, 'menu', 10,   02, 'Countries',         '', 'Country',   '', '#Menu.Countries', true) ,
+  (14, 'menu', 10,   03, 'Reviews',           '', 'Review',    '', '#Menu.Reviews', true),
+  (13, 'sep',  10,   04, '-',                 '', '',          '', '', true) ,
+  (15, 'menu', 10,   05, 'Wines with Reviews','', 'WineParent','', '#Menu.WinesReviews', true),
 
   (20, 'menu', null,  03, 'Configuration',    '', '#',        '', '#Menu.Configuration', false) ,
   (21, 'menu', 20,    01, 'Option1',          '', 'Option1',  '', '#Menu.Option1', false) ,
