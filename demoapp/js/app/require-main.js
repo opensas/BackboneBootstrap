83196b6632f38d6b7c4fe7796d50563e2302d2bc
opensas <opensas@gmail.com>
2013-03-04 00:45:29 -0300

    add moment and bootstrap-datepicker dependencies remove asser, chai and jquery-ui

--- demoapp/js/app/require-main.js
+++ demoapp/js/app/require-main.js
@@ -19,15 +19,17 @@ require.config({
     'requireLib' : 'lib/require.min',
     // Libraries
     jquery       : 'lib/jquery-1.9.1.min',
-    jqueryui     : 'lib/jquery-ui-1.8.21.custom.min',
+    // jqueryui     : 'lib/jquery-ui-1.8.21.custom.min',
+    moment       : 'lib/moment-2.0.0.min',
+    datepicker   : 'lib/bootstrap-datepicker-1.0.1.min',
     gritter      : 'lib/jquery.gritter-1.7.4',
-    lodash       : 'lib/lodash.underscore-1.0.1.min',
+    // lodash       : 'lib/lodash.underscore-1.0.1.min',
+    lodash       : 'lib/lodash.underscore-1.0.1',
     // lodash       : 'lib/underscore-1.4.4.min',
     backbone     : 'lib/backbone-0.9.10',
     // backbone     :'lib/backbone-0.9.10.min',
     bootstrap    : 'lib/bootstrap.min',
     chai         : 'lib/chai-1.4.2.min',
-    assert       : 'src/utils/assert',
     // requirejs plugin
     text         : 'lib/text-2.0.5.min'
   },
